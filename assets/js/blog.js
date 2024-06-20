3ffbf99e4f66b97fd2503dd0740f9a12ad5827f3
bl00dy hazak <kennethhass1014@gmail.com>
2024-06-20 17:42:04 -0400

    added the theme switch in js

---
+++ assets/js/blog.js
@@ -0,0 +1,7 @@
+const themeswitcher = document.querySelector('#theme-switch')
+const container = document.querySelector('.container');
+let mode = 'light'
+
+// I set the variables to reference the theme switch and use the container
+// and created the variable for the light mode that will set the default 
+// settings of the switch.
