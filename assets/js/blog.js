16e42b0536faa40ebc0bdf6fb3c1f368e2031cb7
bl00dy hazak <kennethhass1014@gmail.com>
2024-06-20 18:23:15 -0400

    created blogpost display function and localstorage item list

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -7,3 +7,16 @@ let mode = 'light'
 // I set the variables to reference the theme switch and use the container
 // and created the variable for the light mode that will set the default 
 // settings of the switch.
+
+function displayBlogPost () {
+
+    const blogPost = document.getElementById('blogPost');
+
+// I am setting the blogPost variable so it can be accessed by the
+// function, displayBlogPost.
+    
+    const blogList = JSON.parse(localStorage.getItem('blogList'));
+
+// I am creating a variable to retrieve the information stored in the 
+// localstorage from the blogList and convert it from a JSON string back
+// into an object.
