c1c80d5ac2f075e1d75f809ce2df93e1e15579f4
bl00dy hazak <kennethhass1014@gmail.com>
2024-06-24 14:03:48 -0400

    added the fucntion to create elements, to use input values, and append new elements so they appear on the browser

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -20,3 +20,39 @@ function displayBlogPost () {
 // I am creating a variable to retrieve the information stored in the 
 // localstorage from the blogList and convert it from a JSON string back
 // into an object.
+
+blogList.forEach(element => {
+    const child = document.createElement('div')
+
+    const title = document.createElement('h2')
+
+    const userName = document.createElement('h3')
+
+    const content = document.createElement('p')
+
+// I initiaize the create element in the fuction to make the div, h2,
+// h3 and p elements for the blog post, and assigning these new elemnets
+// to the variable, bloglist. This will set the items to be shown in the
+// document object.
+
+    title.innerHTML = element.title
+
+    title.innerHTML = element.userName
+
+    title.innerHTML = element.content
+
+// This tells the computer that I want the users input values for username,
+// content input and titles to appear in the DOM, when the elements
+// are created.
+    
+    child.appendChild(title)
+
+    child.appendChild(userName)
+
+    child.appendChild(content)
+
+// I tell the computer to append the newly created elements to the 
+// actual DOM that way they will appear on the browser screen 
+   
+blogPost.appendChild(child)
+}); 
